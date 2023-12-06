9edaf9328b3cd9565676f2eb32318b6a3712c802
vidhanpandya23 <vidhanpandya995@gmail.com>
2023-12-06 11:26:35 +0530

    Updates made

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -48,7 +48,7 @@ class DataTransformation:
 
                 steps=[
                     ("imputer",SimpleImputer(strategy="most_frequent")),
-                    ("one_hot_encoder",OneHotEncoder()),
+                    ("one_hot_encoder",OneHotEncoder(handle_unknown='ignore')),
                     ("scaler",StandardScaler(with_mean=False))
                 ]
 
